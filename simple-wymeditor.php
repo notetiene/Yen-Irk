233ac830320d062e6a71422d35f7b1210e58c926
Etienne <yuicopr@gmail.com>
2014-03-13 13:59:50 -0400

    Test

--- simple-wymeditor.php
+++ simple-wymeditor.php
@@ -32,7 +32,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
 Class SimpleWYMeditor {
-	var $version = '0.4.6.1';
+	var $version = '0.4.7';
 
 	var $wymeditor_url = '';
 	var $plugin_url = '';
